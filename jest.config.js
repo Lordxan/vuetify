23be5eece9c3c842a25f921f109abfe647f9a7a1
Lordxan <52027483+Lordxan@users.noreply.github.com>
2022-01-22 17:55:39 +0700

    Added build files

--- jest.config.js
+++ jest.config.js
@@ -1,46 +1,10 @@
+const base = require('../../jest.config')
+
 module.exports = {
-  verbose: false,
-  testEnvironment: 'jest-environment-jsdom-fourteen',
-  roots: [
-    '<rootDir>/src',
-  ],
-  setupFilesAfterEnv: [
-    '<rootDir>/test/index.ts',
-  ],
-  moduleFileExtensions: [
-    'tsx',
-    'ts',
-    'js',
-  ],
-  moduleDirectories: [
-    'node_modules',
-  ],
-  moduleNameMapper: {
-    '^@/test$': '<rootDir>/test/index.js',
-    '^@/test/(.*)$': '<rootDir>/test/$1',
-    '^@/(.*)$': '<rootDir>/src/$1',
-    '\\.(css|sass|scss)$': 'identity-obj-proxy',
-  },
-  transform: {
-    '\\.(styl)$': 'jest-css-modules',
-    '\\.(sass|scss)$': 'jest-css-modules',
-    '.*\\.(j|t)sx?$': 'babel-jest',
-  },
-  collectCoverageFrom: [
-    'src/**/*.{js,ts,tsx}',
-    '!**/*.d.ts',
-  ],
-  transformIgnorePatterns: [
-    'node_modules/(?!vue-router)',
-  ],
-  snapshotSerializers: [
-    'jest-serializer-html',
-  ],
-  testMatch: [
-    // Default
-    '**/test/**/*.js',
-    '**/__tests__/**/*.spec.js',
-    '**/__tests__/**/*.spec.ts',
-    '**/__tests__/**/*.spec.tsx',
+  ...base,
+  name: 'Vuetify',
+  displayName: 'Vuetify',
+  setupFiles: [
+    'jest-canvas-mock',
   ],
 }
--- packages/docs/src/examples/v-btn/prop-block.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div class="fill-height">
-    <v-btn block>
-      Block Button
-    </v-btn>
-  </div>
-</template>
